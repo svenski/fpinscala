e3e3f8c65acf2830e627c21d12f30a5ed39881a2
Sergiusz <sergiuszbleja@gmail.com>
2015-11-01 22:47:30 +0000

    Starting on Tree

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,7 +7,15 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
+  def size[A](t: Tree[A]): Int =  t match {
+      case Leaf(_) => 1
+      case Branch(l, r) => size(l) + size(r) + 1
+  }
+
+  val tt = Branch(Leaf(4), Branch(Leaf(1), Branch(Leaf(3), Leaf(6))
+
+  //def max(t: Tree[Int]) : Int = t match 
 
 
-
+  
 }

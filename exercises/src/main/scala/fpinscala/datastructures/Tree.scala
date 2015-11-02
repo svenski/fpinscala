5fe19a816a9c4c0f1bc1e8d4a303deac157edc87
Sergiusz <sergiusz.bleja@timgroup.com>
2015-11-02 16:53:03 +0000

    Sergiusz: Removing fold

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -12,10 +12,26 @@ object Tree {
       case Branch(l, r) => size(l) + size(r) + 1
   }
 
-  val tt = Branch(Leaf(4), Branch(Leaf(1), Branch(Leaf(3), Leaf(6))
+  val tt = Branch(Leaf(4), Branch(Leaf(1), Branch(Leaf(3), Leaf(6))))
 
-  //def max(t: Tree[Int]) : Int = t match 
+  def max(t: Tree[Int]) : Int = t match {
+    case Leaf(v) => v
+    case Branch(l,r) => max(l).max(max(r))
+  }
 
-
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(l,r) => 1 + depth(l).max(depth(r))
+  }
   
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] =  t match {
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+    case Leaf(v) => Leaf(f(v))
+  }
+
+//  def fold[A,B](t: Tree[A], z: B)(f: (A,B) => B): B = t match {
+//   case Leaf(v) => f(v, z)
+//    case Branch(l, r) => fold(r, fold(l, z)(f))(f)
+//  }
+
 }
